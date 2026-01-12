6f635caa1505f45a4baeebf564cef6b6e6dad2f1
Danil <Danil@DESKTOP-99KGHPD>
2026-01-12 16:54:56 +1000

    WaterVehicle DONE

--- AbstractBaseClass/main.cpp
+++ AbstractBaseClass/main.cpp
@@ -9,14 +9,61 @@ class Vehicle
 public:
 	virtual void move() = 0;
 };
-class GroundVehicle : public Vehicle {};
 
-class car : public GroundVehicle
+
+class GroundVehicle : public Vehicle {};
+class Car : public GroundVehicle
 {
 public:
 	void move()override
 	{
-		cout << "Машинка едет на 2 колесах" << endl;
+		cout << "Машинка едет на 4 колесах" << endl;
+	}
+};
+class Bike :public GroundVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Велосипед едет на 2 колесах" << endl;
+	}
+};
+
+
+class AirVehicle : public Vehicle {};
+class Plane : public AirVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Самолет летит с помощью турбин и крыльев" << endl;
+	}
+};
+class Helicopter : public AirVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Вертолет летит с помощью несущего винта" << endl;
+	}
+};
+
+
+class WaterVehicle : public Vehicle{};
+class SailingShip : public WaterVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Парусные корабли перемещаются c помощью парусов" << endl;
+	}
+};
+class Submarine : public WaterVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Подводные лодки перемещаются c гребных винтов, водометов, электромоторов и тд" << endl;
 	}
 };
 
